849842da6c06ca9b9ac7c67eb4d140c7a13a0192
blockchainer2022 <=blockchainer.2202@gmail.com>
2021-09-30 22:12:44 +0530

    chainid changed

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ function App() {
     // you are connected to main net
     // Please connect to main net
 
-    if (chainId === 4) {
+    if (chainId === 1) {
       toast(`You are connected to main net`, {
         type: "success",
         position: toast.POSITION.BOTTOM_CENTER,
@@ -128,7 +128,7 @@ function App() {
 
   async function mint(mintCount) {
     if (contract) {
-      if (chainId === 4) {
+      if (chainId === 1) {
         if (mintCount === 0) {
           setLessMintAmountAlert(true);
         } else {
